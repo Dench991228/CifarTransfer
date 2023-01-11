ca4c94ccfc8d5756ea5f78cf82ffd0808f567e76
Dench991228 <denghanzhi85139@163.com>
2023-01-11 18:53:39 +0800

    完成了第一个版本，准备线上测试

--- TransferVit.py
+++ TransferVit.py
@@ -1,14 +1,27 @@
 from pytorch_pretrained_vit import ViT
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 
 class TVit(ViT):
-    def __init__(self, backbone_embedding, count_classes):
-        super().__init__()
-        self.linear = nn.Linear(backbone_embedding, count_classes)
+    def __init__(self, name, img_size, backbone_embedding, count_classes):
+        super().__init__(name, pretrained=True, image_size=img_size)  # 先初始化一个骨干模型
+        self.classifier = nn.Linear(backbone_embedding, count_classes, bias=False)
 
     def forward(self, x):
-        x = self.linear(x)
+        b, c, fh, fw = x.shape
+        x = self.patch_embedding(x)  # b,d,gh,gw
+        x = x.flatten(2).transpose(1, 2)  # b,gh*gw,d
+        if hasattr(self, 'class_token'):
+            x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw+1,d
+        if hasattr(self, 'positional_embedding'):
+            x = self.positional_embedding(x)  # b,gh*gw+1,d
+        x = self.transformer(x)  # b,gh*gw+1,d
+        if hasattr(self, 'pre_logits'):
+            x = self.pre_logits(x)
+            x = torch.tanh(x)
+        x = self.norm(x)[:, 0]  # b,d
+        x = self.classifier(x)
         x = F.softmax(x, dim=1)
         return x
--- main.py
+++ main.py
@@ -9,26 +9,19 @@ import torch.optim
 
 args = argparse.ArgumentParser(description="Transferring ViT to CIFAR10 or CIFAR100")
 args.add_argument("--dataset", default='cifar10')
-model = ViT('B_16_imagenet1k', pretrained=True)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# 把后面的分类头去掉，换成我自己的分类头
-backboneViT = nn.Sequential(*list(model.children())[:-1])
-backboneViT.to(device)
-print("Backbone Vision Transformer prepared")
 
 
-def train_one_epoch(backbone, head, criterion, optimizer, train_loader):
+def train_one_epoch(model, criterion, optimizer, train_loader):
     avg_loss = 0
     count_correct = 0
     count_wrong = 0
-    backbone.train()
-    head.train()
+    model.train()
     for idx, (images, targets) in enumerate(train_loader):
         images, targets = images.to(device), targets.to(device)
-        # 先输出得到backbone部分的嵌入式表达
-        outputs = backbone(images)[:, 0]
-        # 然后是分类器的表达
-        outputs = head(outputs)
+        # 得到图片的分类归一化概率
+        print(images.shape)
+        outputs = model(images)
         # 得到损失函数
         loss = criterion(outputs, targets)
         avg_loss += loss
@@ -46,16 +39,14 @@ def train_one_epoch(backbone, head, criterion, optimizer, train_loader):
     print(f"train acc: {avg_correct}")
 
 
-def test(backbone, head, criterion, test_loader):
+def test(model, criterion, test_loader):
     loss = 0
     correct = 0
     wrong = 0
-    backbone.eval()
-    head.eval()
+    model.eval()
     for idx, (images, targets) in enumerate(test_loader):
         images, targets = images.to(device), targets.to(device)
-        outputs = backbone(images)[:, 0]
-        outputs = head(outputs)
+        outputs = model(images)
         batch_loss = criterion(outputs)
         loss += batch_loss
         predicts = torch.argmax(outputs, dim=1)
@@ -71,16 +62,22 @@ if __name__ == '__main__':
     args = args.parse_args()
     train_loader = get_train_loader(args.dataset)
     test_loader = get_test_loader(args.dataset)
-    classifier_head = TVit(768, 10 if args.dataset == 'cifar10' else 100)
-    classifier_head.to(device)
     learning_rate = 1e-2
     transfer_ratio = 1e-2
-    parameters = [{'params': backboneViT.parameters(), 'lr': learning_rate * transfer_ratio},
-                  {'params': classifier_head.parameters()}]
+    model = TVit('B_16', img_size=224, backbone_embedding=768, count_classes=10)
+    model.to(device)
+    print("pretrained model loaded")
+    state_dict = model.state_dict()
+    del state_dict['classifier.weight']
+    fine_tuned_params = []
+    for k in state_dict:
+        fine_tuned_params.append(model.get_parameter(k))
+    parameters = [{'params': fine_tuned_params, 'lr': learning_rate * transfer_ratio},
+                  {'params': [model.classifier.weight], 'lr': learning_rate}]
     optimizer = torch.optim.SGD(parameters, lr=learning_rate, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, eta_min=2e-4, T_max=60)
     criterion = nn.CrossEntropyLoss()
     for id in range(60):
         print(f"Epoch {id}")
-        train_one_epoch(backboneViT, classifier_head, criterion, optimizer, get_train_loader(args.dataset))
-        test(backboneViT, classifier_head, criterion, get_test_loader(args.dataset))
+        train_one_epoch(model, criterion, optimizer, get_train_loader(args.dataset))
+        test(model, criterion, get_test_loader(args.dataset))
